89bf3804ba90554c4b176f744c563fa9679038c8
Akhsell <m.akhseldwinanda.16@gmail.com>
2026-01-22 14:11:01 +0700

    Made Migrations & Models

---
+++ app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'content',
+        'image',
+        'price',
+        'is_active',
+        'order',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+        'price' => 'decimal:2'
+    ];
+}
